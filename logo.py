82cae41efc1cfcc9f61e50e25bfab22dcf0f3943
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-04-22 17:18:00 +0700

    Update logo.py

--- logo.py
+++ logo.py
@@ -26,10 +26,9 @@ def urut(i,t):
 #--> Logo
 def logo():
     sp = ' '*7
-  l1 = ('     %s  _________       __                          '%(P))
-    l2 = ('     %s /   %s_____%s/%s____  %s|  | %s__ ________________     '%(M,P,M,P,M,P))
-    l3 = ('     %s \_____  \\\__  \ %s|  |/ // %s__ \_  __ \__  \   '%(P,M,P))
-    l4 = ('     %s /        %s\\%s/%s __ \\%s|    <%s\  ___%s/| | %s\\%s// %s___ \   '%(M,P,M,P,M,P,M,P,M,P))
-    l5 = ('     %s/%s_________%s(%s______%s/%s__%s|%s__\\_____%s>%s__%s|  (%s_______\\'%(M,P,M,P,M,P,M,P,M,P,M,P))
-    l6 = ('     %s Multi Brute Force Facebook %s%s %sBy %sDenventa     '%(P,M,Version,P,M))
-    print('%s\n%s\n%s\n%s\n%s\n%s'%(l1,l2,l3,l4,l5,l6))
+    print('     %s  _________       __                          '%(P))
+    print('     %s /   %s_____%s/%s____  %s|  | %s__ ________________     '%(sp,P,M,P,M,P))
+    print('     %s \_____  \\\__  \ %s|  |/ // %s__ \_  __ \__  \   '%(sp,M,P))
+    print('     %s /        %s\\%s/%s __ \\%s|    <%s\  ___%s/| | %s\\%s// %s___ \   '%(sp,P,M,P,M,P,M,P,M,P))
+    print('     %s/%s_________%s(%s______%s/%s__%s|%s__\\_____%s>%s__%s|  (%s_______\\'%(sp,P,M,P,M,P,M,P,M,P,M,P))
+    print('     %s Multi Brute Force Facebook %s%s %sBy %sDenventa     '%(sp,M,Version,P,M))
