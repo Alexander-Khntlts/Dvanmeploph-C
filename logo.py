8cd188c22cabc0eb4bb76bcf05deaa37194a437f
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-04-22 17:16:32 +0700

    Update logo.py

--- logo.py
+++ logo.py
@@ -3,7 +3,7 @@ Author    = 'Dapunta Khurayra X'
 Developer = 'Denventa Ferly Afriliyan'
 Facebook  = 'Facebook.com/Denventa.Xayonara.Team.UnlimitedARMY'
 Instagram = 'Instagram.com/afriliyanferlly_shishigami'
-
+Version = '0.1'
 #--> oImprt Module
 import sys, time
 
@@ -19,18 +19,6 @@ P = '\x1b[38;5;231m' # Putih
 J = '\x1b[38;5;208m' # Jingga
 A = '\x1b[38;5;248m' # Abu-Abu
 
-###----------[ RICH COLOR STYLE ]---------- ###
-Z2 = "[#000000]" # Hitam
-M2 = "[#FF0000]" # Merah
-H2 = "[#00FF00]" # Hijau
-K2 = "[#FFFF00]" # Kuning
-B2 = "[#00C8FF]" # Biru
-U2 = "[#AF00FF]" # Ungu
-N2 = "[#FF00FF]" # Pink
-O2 = "[#00FFFF]" # Biru Muda
-P2 = "[#FFFFFF]" # Putih
-J2 = "[#FF8F00]" # Jingga
-A2 = "[#AAAAAA]" # Abu-Abu
 #--> Animasi
 def urut(i,t):
     for e in i + '\n': sys.stdout.write(e); sys.stdout.flush(); time.sleep(t)
@@ -38,12 +26,10 @@ def urut(i,t):
 #--> Logo
 def logo():
     sp = ' '*7
-  Tampilan_Logo = f"""
-{M2}________                                    __          
-{M2}\______ \   ____   _______  __ ____   _____/  |{U2}______   
- {M2}|    |  \_/ __ \ /    \  \/ // __ \ /    \   __{U2}\__  \  
- {M2}|    `   \  ___/|   |  \   /\  ___/|   |  \  |  {U2}/ __ \_
-{M2}/_______  /\___  >___|  /\_/  \___  >___|  /__| {U2}(____  /
-        {M2}\/     \/     \/          \/     \/          {U2}\/"""
-
-        
+  l1 = ('     %s  _________       __                          '%(P))
+    l2 = ('     %s /   %s_____%s/%s____  %s|  | %s__ ________________     '%(M,P,M,P,M,P))
+    l3 = ('     %s \_____  \\\__  \ %s|  |/ // %s__ \_  __ \__  \   '%(P,M,P))
+    l4 = ('     %s /        %s\\%s/%s __ \\%s|    <%s\  ___%s/| | %s\\%s// %s___ \   '%(M,P,M,P,M,P,M,P,M,P))
+    l5 = ('     %s/%s_________%s(%s______%s/%s__%s|%s__\\_____%s>%s__%s|  (%s_______\\'%(M,P,M,P,M,P,M,P,M,P,M,P))
+    l6 = ('     %s Multi Brute Force Facebook %s%s %sBy %sDenventa     '%(P,M,Version,P,M))
+    print('%s\n%s\n%s\n%s\n%s\n%s'%(l1,l2,l3,l4,l5,l6))
